9f109379fe1a259b319bcc798117e3d0b58926e3
adamd87 <adam.drozdz1987@gmail.com>
2022-04-03 04:05:33 +0200

    init

---
+++ crm-srv/src/test/java/pl/adamd/crmsrv/CrmSrvApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.adamd.crmsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CrmSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
